4f71ab74e286c6fef529dc8abee03dfdc983b4fb
Christopher Augg <chrisa39@uw.edu>
2016-02-26 13:20:28 -0800

    Initial Commit

---
+++ testSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testSearch
+//
+//  Created by Chris Augg on 2/26/16.
+//  Copyright © 2016 Auggie Doggie iOSware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
